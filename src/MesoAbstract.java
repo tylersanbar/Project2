230e2dbe3381b3c0dcfd31cce6acf104faf4e21e
Tyler Sanbar <tylersanbar@ou.edu>
2019-10-08 08:52:38 -0500

    Adding /src, was mistakenly a subfolder

---
+++ src/MesoAbstract.java
@@ -0,0 +1,13 @@
+/**
+ * @author Mohammad Mukhtaruzzaman
+ * @version 2019-09-18
+*/
+public abstract class MesoAbstract 
+{
+	//Abstract method calAverage to return integer array.
+	abstract int[] calAverage();
+	
+	//Abstract method letterAverage to return char.
+	abstract char letterAverage();
+	
+}
---
+++ src/PosAvg.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PosAvg {
+	
+	private String fileName = "Mesonet.txt";
+	ArrayList<MesoStation> stationList = new ArrayList<MesoStation>();
+	
+	public PosAvg(String stID) {
+		try {
+			read(fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void read(String filename) throws IOException
+    {
+    	
+    	BufferedReader br = new BufferedReader(new FileReader(filename));
+    	
+    	br.readLine();
+    	br.readLine();
+    	br.readLine();
+    	br.readLine();
+    	br.readLine();
+    	br.readLine();
+    	String strg = br.readLine();
+    	
+    	//Adds stid, then loops through remaining lines
+    	while (strg != null) {
+    		strg = strg.substring(4, 8);
+    		MesoStation station = new MesoStation(strg);
+    		stationList.add(station);
+    		strg = br.readLine();
+    	}
+    	br.close();
+    }
+}
+
+	
